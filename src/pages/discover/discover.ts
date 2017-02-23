5f1549b2a1bf6e685c51be0b320977eeffb72f17
Vito Cairone <vitocairone@gmail.com>
2017-02-23 12:58:36 -0800

    likes and saw persisted

--- src/pages/discover/discover.ts
+++ src/pages/discover/discover.ts
@@ -3,7 +3,7 @@ import { NavController, NavParams, ToastController } from 'ionic-angular';
 import { DiscoverService } from '../../providers/discover-service';
 import { UserService } from '../../providers/user-service';
 import { ChatsPage } from '../chats/chats';
- 
+
 import {
   StackConfig,
   Stack,
@@ -11,145 +11,146 @@ import {
   ThrowEvent,
   DragEvent,
   SwingStackComponent,
-  SwingCardComponent } from 'angular2-swing';
+  SwingCardComponent 
+} from 'angular2-swing';
 
 @Component({
-    selector: 'page-discover',
-    templateUrl: 'discover.html'
+  selector: 'page-discover',
+  templateUrl: 'discover.html'
 })
 export class DiscoverPage {
-    @ViewChild('swingContainer') swingStack: SwingStackComponent;
-    @ViewChildren('cardsContainer') swingCards: QueryList<SwingCardComponent>;
-    
-    users: Array<any>;
-    undoHistory: Array<any>;
-    stackConfig: StackConfig;
-  
-    constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController, private discoverS: DiscoverService, private userS: UserService) {
-      this.stackConfig = {
-        throwOutConfidence: (offset, element) => {
-          return Math.min(Math.abs(offset) / (element.offsetWidth/2), 1);
-        },
-        transform: (element, x, y, r) => {
-          this.onItemMove(element, x, y, r);
-        },
-        throwOutDistance: (d) => {
-          return 800;
-        }
-      };
-      this.users = [];
-      this.undoHistory = [];
+  @ViewChild('swingContainer') swingStack: SwingStackComponent;
+  @ViewChildren('cardsContainer') swingCards: QueryList<SwingCardComponent>;
+
+  users: Array<any>;
+  undoHistory: Array<any>;
+  stackConfig: StackConfig;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController, private discoverS: DiscoverService, private userS: UserService) {
+    this.stackConfig = {
+      throwOutConfidence: (offset, element) => {
+        return Math.min(Math.abs(offset) / (element.offsetWidth/2), 1);
+      },
+      transform: (element, x, y, r) => {
+        this.onItemMove(element, x, y, r);
+      },
+      throwOutDistance: (d) => {
+        return 800;
+      }
+    };
+    this.users = [];
+    this.undoHistory = [];
+  }
+
+  ionViewDidLoad() {
+    // Either subscribe in controller or set in HTML
+    this.swingStack.throwin.subscribe((event: DragEvent) => {
+      event.target.style.background = '#fff';
+    });
+  }
+
+  ionViewWillEnter() {
+    console.log("Entered discover page");
+    this.fetchUsers();
+  }
+
+  openChats() {
+    this.navCtrl.push(ChatsPage);
+  }
+
+  presentToast(message) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 500,
+      position: 'top'
+    });
+    toast.present();
+  }
+
+  fetchUsers() {
+    console.log("fetching visible users");
+    this.discoverS.fetchVisibleUsers().then(data => {
+      console.log("fetch returned visible users");
+      console.log(data);
+      this.users = data;
+    }).catch(error => {
+      alert(error);
+    });
+  }
+
+  swipeLeft() {
+    console.log("Swiping left...");
+    console.log(this.users.length);
+    if (this.users.length > 0) {
+      let currentCard = this.users.pop();
+      this.undoHistory.push(currentCard);
+      console.log(currentCard);
+      this.discoverS.saw(currentCard.id).then(success => {
+        this.presentToast('You did not like: ' + currentCard.firstName);
+      }).catch(error => {
+        this.presentToast('Error saving swipe');
+      });
     }
-    
-    ionViewDidLoad() {
-        // Either subscribe in controller or set in HTML
-        this.swingStack.throwin.subscribe((event: DragEvent) => {
-	       event.target.style.background = '#fff';
-        });
-	}
-    
-    ionViewWillEnter() {
-        console.log("Entered discover page");
-        this.fetchUsers();
-    }
-    
-    openChats() {
-        this.navCtrl.push(ChatsPage);
-    }
-    
-    presentToast(message) {
-        let toast = this.toastCtrl.create({
-            message: message,
-            duration: 500,
-            position: 'top'
-        });
-        toast.present();
-    }
-    
-    fetchUsers() {
-        console.log("fetching visible users");
-        // this.userS.fetchGlobalUsers().then(data => {
-        //     console.log("fetch returned global users");
-        this.discoverS.fetchVisibleUsers().then(data => {
-            console.log("fetch returned visible users");
-            console.log(data);
-            this.users = data;
+  }
+
+  swipeRight() {
+    console.log("Swiping right...");
+    console.log(this.users.length);
+    if (this.users.length > 0) {
+      let currentCard = this.users.pop();
+      this.undoHistory.push(currentCard);
+      console.log(currentCard);
+      this.discoverS.saw(currentCard.id).then(success => {
+        this.discoverS.liked(currentCard.id).then(success => {
+          this.presentToast('You liked: ' + currentCard.firstName);
         }).catch(error => {
-            alert(error);
+          this.presentToast('Error saving swipe');
         });
+      }).catch(error => {
+        this.presentToast('Error saving swipe');
+      });
     }
-    
-    swipeLeft() {
-        console.log("Swiping left...");
-        console.log(this.users.length);
-        if (this.users.length > 0) {
-            let currentCard = this.users.pop();
-            this.undoHistory.push(currentCard);
-            console.log(currentCard);
-            this.discoverS.saw(currentCard.id).then(success => {
-                this.presentToast('You did not like: ' + currentCard.firstName);
-            }).catch(error => {
-                this.presentToast('Error saving swipe');
-            });
-        }
+  }
+
+  doubleLike() {
+    this.presentToast("You double liked " + this.users[0].firstName );
+
+  }
+
+  undo() {
+    this.presentToast("Undo pressed!");
+    if (this.undoHistory.length > 0) {
+      let undoCard = this.undoHistory.pop();
+      this.users.push(undoCard);
     }
-    
-	swipeRight() {
-        console.log("Swiping right...");
-        console.log(this.users.length);
-        if (this.users.length > 0) {
-            let currentCard = this.users.pop();
-            this.undoHistory.push(currentCard);
-            console.log(currentCard);
-            this.discoverS.saw(currentCard.id).then(success => {
-                this.discoverS.liked(currentCard.id).then(success => {
-                    this.presentToast('You liked: ' + currentCard.firstName);
-                }).catch(error => {
-                    this.presentToast('Error saving swipe');
-                });
-            }).catch(error => {
-                this.presentToast('Error saving swipe');
-            });
-        }
-	}
-    
-    doubleLike() {
-        this.presentToast("You double liked " + this.users[0].firstName );
-        
-    }
-    
-    undo() {
-        this.presentToast("Undo pressed!");
-        let undoCard = this.undoHistory.pop();
-        this.users.push(undoCard);
+  }
+
+  // Called whenever we drag an element
+  onItemMove(element, x, y, r) {
+    var color = '';
+    var abs = Math.abs(x);
+    let min = Math.trunc(Math.min(16*16 - abs, 16*16));
+    let hexCode = this.decimalToHex(min, 2);
+
+    if (x < 0) {
+      color = '#FF' + hexCode + hexCode;
+    } else {
+      color = '#' + hexCode + 'FF' + hexCode;
     }
 
-    // Called whenever we drag an element
-	onItemMove(element, x, y, r) {
-	  var color = '';
-	  var abs = Math.abs(x);
-	  let min = Math.trunc(Math.min(16*16 - abs, 16*16));
-	  let hexCode = this.decimalToHex(min, 2);
-	  
-	  if (x < 0) {
-	    color = '#FF' + hexCode + hexCode;
-	  } else {
-	    color = '#' + hexCode + 'FF' + hexCode;
-	  }
-	  
-	  element.style.background = color;
-	  element.style['transform'] = `translate3d(0, 0, 0) translate(${x}px, ${y}px) rotate(${r}deg)`;
-	}
+    element.style.background = color;
+    element.style['transform'] = `translate3d(0, 0, 0) translate(${x}px, ${y}px) rotate(${r}deg)`;
+  }
 
-	// http://stackoverflow.com/questions/57803/how-to-convert-decimal-to-hex-in-javascript
-	decimalToHex(d, padding) {
-	  var hex = Number(d).toString(16);
-	  padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
-	  
-	  while (hex.length < padding) {
-	    hex = "0" + hex;
-	  }
-	  
-	  return hex;
-	}
+  // http://stackoverflow.com/questions/57803/how-to-convert-decimal-to-hex-in-javascript
+  decimalToHex(d, padding) {
+    var hex = Number(d).toString(16);
+    padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
+
+    while (hex.length < padding) {
+      hex = "0" + hex;
+    }
+
+    return hex;
+  }
 }
--- src/providers/discover-service.ts
+++ src/providers/discover-service.ts
@@ -45,64 +45,64 @@ export class DiscoverService {
 
     saw(uid): Promise<any> {
         console.log("saw user");
-        var data = {};
-        data[uid] = true;
         return new Promise((resolve, reject) => {
-            let ref = firebase.database().ref('/user_saw/' + this.userS.user.uid);
-            ref.update(data).then( data => {
-                console.log("user saw saved to db");
-                return ref.once('value');
-            }).then( snapshot => {
-                console.log("user saw returned from db");
-                let val = snapshot.val();
-                console.log("snapshot val: " + JSON.stringify(val));
-                resolve(val);
-            }).catch(error => {
-                console.log(error);
-                reject(error);
-            });
+          var data = {};
+          data[uid] = (new Date).getTime();
+          let ref = firebase.database().ref('/user_saw/' + this.userS.user.id);
+          ref.update(data).then( data => {
+              console.log("user saw saved to db");
+              return ref.once('value');
+          }).then( snapshot => {
+              console.log("user saw returned from db");
+              let val = snapshot.val();
+              console.log("snapshot val: " + JSON.stringify(val));
+              resolve(val);
+          }).catch(error => {
+              console.log(error);
+              reject(error);
+          });
         });
     }
     
     liked(uid): Promise<any> {
         console.log("liking user");
-        var data = {};
-        data[uid] = true;
         return new Promise((resolve, reject) => {
-            let ref = firebase.database().ref('/user_liked/' + this.userS.user.uid);
-            ref.update(data).then( data => {
-                console.log("user liked saved to db");
-                return ref.once('value');
-            }).then( snapshot => {
-                console.log("user liked returned from db");
-                let val = snapshot.val();
-                console.log("snapshot val: " + JSON.stringify(val));
-                resolve(val);
-            }).catch(error => {
-                console.log(error);
-                reject(error);
-            });
+          var data = {};
+          data[uid] = (new Date).getTime();
+          let ref = firebase.database().ref('/user_liked/' + this.userS.user.id);
+          ref.update(data).then( data => {
+              console.log("user liked saved to db");
+              return ref.once('value');
+          }).then( snapshot => {
+              console.log("user liked returned from db");
+              let val = snapshot.val();
+              console.log("snapshot val: " + JSON.stringify(val));
+              resolve(val);
+          }).catch(error => {
+              console.log(error);
+              reject(error);
+          });
         });
     }
     
     doubleLiked(uid): Promise<any> {
         console.log("double liking user");
-        var data = {};
-        data[uid] = true;
         return new Promise((resolve, reject) => {
-            let ref = firebase.database().ref('/user_liked/' + this.userS.user.uid);
-            ref.update(data).then( data => {
-                console.log("user double like saved to db");
-                return ref.once('value');
-            }).then( snapshot => {
-                console.log("user double like returned from db");
-                let val = snapshot.val();
-                console.log("snapshot val: " + JSON.stringify(val));
-                resolve(val);
-            }).catch(error => {
-                console.log(error);
-                reject(error);
-            });
+          var data = {};
+          data[uid] = (new Date).getTime();
+          let ref = firebase.database().ref('/user_liked/' + this.userS.user.id);
+          ref.update(data).then( data => {
+              console.log("user double like saved to db");
+              return ref.once('value');
+          }).then( snapshot => {
+              console.log("user double like returned from db");
+              let val = snapshot.val();
+              console.log("snapshot val: " + JSON.stringify(val));
+              resolve(val);
+          }).catch(error => {
+              console.log(error);
+              reject(error);
+          });
         });
     }
 
